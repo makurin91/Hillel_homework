bbc930300206817c6cce35663969aba789bc5731
mak <makurin.andrey91@gmail.com>
2022-04-21 23:30:55 +0300

    add checking for digits

--- Lesson_8/home_work_classes.py
+++ Lesson_8/home_work_classes.py
@@ -1,3 +1,5 @@
+import functools
+
 '''
 Реализовать класс который бы производил разные операции над 2мя переданными числами.
 
@@ -11,26 +13,42 @@ print(proccess_input.divide()).  выведет 2
 '''
 
 
+def checking_division(func):
+    @functools.wraps(func)
+    def wrap(*args):
+        try:
+            print(int(func(*args)))
+        except TypeError:
+            return 'You need to enter only digits'
+    return wrap
+
+
 class Calculate:
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
+    @checking_division
     def add_func(self):
         return self.a + self.b
 
+    @checking_division
     def subtract_func(self):
         return self.a - self.b
 
     def multiple_func(self):
-        return self.a * self.b
+        if self.a and self.b is int:
+            return self.a * self.b
+        else:
+            return 'You need to enter only digits'
 
+    @checking_division
     def divide_func(self):
         return self.a // self.b
 
 
 class Test:
-    cl = Calculate(4, 1)
+    cl = Calculate(10, 's')
     print(cl.add_func())
     print(cl.subtract_func())
     print(cl.multiple_func())
